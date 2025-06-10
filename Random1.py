216f2bd3ea74fba38e80d296a574a45028449797
Gokudera2025 <si.tuma@duocuc.cl>
2025-06-10 14:23:19 -0400

    FuncionRandom

---
+++ Random1.py
@@ -0,0 +1,9 @@
+import random 
+
+def generar_numero_azar(inicio=1, fin=100):
+
+    return random.randint(inicio, fin)
+
+if __name__ == "__main__":
+    numero = generar_numero_azar()
+    print(f"Número generado al azar: {numero}")
